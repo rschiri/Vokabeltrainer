793ee45030a551b1b38389b5392024cab5487778
PandaProtection <48756651+PandaProtection@users.noreply.github.com>
2020-06-21 13:41:10 +0200

    Add files via upload

--- Vokabeltrainer/addvocable.cpp
+++ Vokabeltrainer/addvocable.cpp
@@ -1,24 +1,73 @@
 #include "addvocable.h"
 #include "ui_addvocable.h"
 
-addVocable::addVocable(QWidget *parent) :
+#include <QTextStream>
+#include <QMessageBox>
+#include <QDebug>
+#include <QSqlError>
+
+#include <databasequery.h>
+
+AddVocable::AddVocable(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::addVocable)
 {
     ui->setupUi(this);
+    Databasequery dbq;
+    ui->deutschWortartComboBox->addItems(dbq.getAllWordtypes());
+    ui->spracheWortartComboBox->addItems(dbq.getAllWordtypes());
+    ui->spracheComboBox->addItems(dbq.getAllLanguagesExcept("Deutsch"));
+    ui->kategorieComboBox->addItems(dbq.getAllCategories());
 
-    /*ui->deutschWortartComboBox->addItems();
-     *
-     *
-     * */
 }
 
-addVocable::~addVocable()
+AddVocable::~AddVocable()
 {
     delete ui;
 }
 
-void addVocable::on_hinzufuegenButton_clicked()
+void AddVocable::on_hinzufuegenButton_clicked()
 {
+    QString stringGermanWords = ui->deutschBedeutung1LineEdit->text();
+    if(!stringGermanWords.isEmpty() && !ui->deutschBedeutung2LineEdit->text().isEmpty())
+        stringGermanWords =+ "," + ui->deutschBedeutung2LineEdit->text();
+    if(!stringGermanWords.isEmpty() && !ui->deutschBedeutung3LineEdit->text().isEmpty())
+        stringGermanWords =+ "," + ui->deutschBedeutung3LineEdit->text();
+     Databasequery dbq;
+     int wordid1= 0;
+     int wordid2= 0;
+    if(!stringGermanWords.isEmpty()){
+        wordid1 = dbq.addWord(stringGermanWords,ui->deutschWortartComboBox->currentText(),"Deutsch");
+    }
+    QString stringLanguageWords = ui->spracheBedeutung1LineEdit->text();
+    if(!stringLanguageWords.isEmpty() && !ui->spracheBedeutung2LineEdit->text().isEmpty())
+        stringLanguageWords =+ "," + ui->spracheBedeutung2LineEdit->text();
+    if(!stringLanguageWords.isEmpty() && !ui->spracheBedeutung3LineEdit->text().isEmpty())
+        stringLanguageWords =+ "," + ui->spracheBedeutung3LineEdit->text();
 
+    if(!stringLanguageWords.isEmpty()){
+        wordid2 = dbq.addWord(stringLanguageWords,ui->spracheWortartComboBox->currentText(),ui->spracheComboBox->currentText());
+    }
+
+    if(wordid1 > 0 || wordid2 > 0){
+        dbq.addVocable(wordid1,wordid2,ui->kategorieComboBox->currentText());
+        QMessageBox msgbox;
+        msgbox.setText("Vocable wurde hinzugefügt");
+        msgbox.exec();
+    }
+
+}
+
+void AddVocable::on_resetButton_clicked()
+{
+    ui->deutschBedeutung1LineEdit->clear();
+    ui->deutschBedeutung2LineEdit->clear();
+    ui->deutschBedeutung3LineEdit->clear();
+    ui->spracheBedeutung1LineEdit->clear();
+    ui->spracheBedeutung2LineEdit->clear();
+    ui->spracheBedeutung3LineEdit->clear();
+    ui->deutschWortartComboBox->setCurrentIndex(0);
+    ui->spracheWortartComboBox->setCurrentIndex(0);
+    ui->spracheComboBox->setCurrentIndex(0);
+    ui->kategorieComboBox->setCurrentIndex(0);
 }
---
+++ Vokabeltrainer/databasequery.cpp
@@ -0,0 +1,135 @@
+#include "databasequery.h"
+#include "databaseconnection.h"
+
+#include <QTextStream>
+
+Databasequery::Databasequery()
+{
+
+}
+
+QStringList Databasequery::getAllCategories(){
+    QStringList qStringList;
+
+    DatabaseConnection dbc;
+    dbc.openConnection();
+    QSqlQuery *qry = new QSqlQuery(dbc.db);
+
+    qry->exec("select category from category");
+
+    while (qry->next()) {
+       QString mystring = qry->value(0).toString();
+       qStringList.push_back(mystring);
+        }
+
+    dbc.closeConnection();
+    return qStringList;
+}
+
+QStringList Databasequery::getAllWordtypes(){
+    QStringList qStringList;
+
+    DatabaseConnection dbc;
+    dbc.openConnection();
+    QSqlQuery *qry = new QSqlQuery(dbc.db);
+
+    qry->exec("select wordtype from wordtype");
+
+    while (qry->next()) {
+       QString mystring = qry->value(0).toString();
+       qStringList.push_back(mystring);
+        }
+
+    dbc.closeConnection();
+    return qStringList;
+}
+
+QStringList Databasequery::getAllLanguages(){
+    QStringList qStringList;
+
+    DatabaseConnection dbc;
+    dbc.openConnection();
+    QSqlQuery *qry = new QSqlQuery(dbc.db);
+
+    qry->exec("select language from languages");
+
+    while (qry->next()) {
+       QString mystring = qry->value(0).toString();
+       qStringList.push_back(mystring);
+        }
+
+    dbc.closeConnection();
+    return qStringList;
+}
+
+QStringList Databasequery::getAllLanguagesExcept(QString qstring){
+    QStringList qStringList;
+
+    DatabaseConnection dbc;
+    dbc.openConnection();
+    QSqlQuery *qry = new QSqlQuery(dbc.db);
+
+    qry->exec("select language from languages except select language from languages where language = '"+ qstring +"'");
+
+    while (qry->next()) {
+       QString mystring = qry->value(0).toString();
+       qStringList.push_back(mystring);
+        }
+
+    dbc.closeConnection();
+    return qStringList;
+}
+
+
+int Databasequery::getIDCategory(DatabaseConnection &dbc,QString category){
+    QSqlQuery *qry = new QSqlQuery(dbc.db);
+    return qry->exec("select categoryid from category where category = '"+ category +"' ");
+}
+
+int Databasequery::getIDWordtype(DatabaseConnection &dbc,QString wordtype){
+    QSqlQuery *qry = new QSqlQuery(dbc.db);
+    return qry->exec("select categoryid from category where category = '"+ wordtype +"' ");
+}
+
+int Databasequery::getIDLanguage(DatabaseConnection &dbc,QString language){
+    QSqlQuery *qry = new QSqlQuery(dbc.db);
+    return qry->exec("select categoryid from category where category = '"+ language +"' ");
+}
+
+int Databasequery::addWord(QString statement,QString wordtype,QString language){
+    DatabaseConnection dbc;
+    int wordid = 0;
+    dbc.openConnection();
+    QSqlQuery *qry = new QSqlQuery(dbc.db);
+
+    qry->prepare("INSERT INTO word (wordtypeid, languageid, word)"
+                 "VALUES (:wordtypeid, :languageid, '{ "+ statement + " }') returning wordid");
+    qry->bindValue(":wordtypeid", getIDWordtype(dbc,wordtype));
+    qry->bindValue(":languageid", getIDLanguage(dbc,language));
+    wordid = qry->exec();
+
+    dbc.closeConnection();
+    return wordid;
+}
+
+void Databasequery::addVocable(int wordid1, int wordid2, QString category){
+    DatabaseConnection dbc;
+    dbc.openConnection();
+    QSqlQuery *qry = new QSqlQuery(dbc.db);
+
+    qry->prepare("INSERT INTO vocable (wordid1,wordid2,categoryid)"
+              "VALUES (:word1,:word2,:category)");
+    qry->bindValue(":word1",wordid1);
+    qry->bindValue(":word2",wordid2);
+    qry->bindValue(":category",getIDCategory(dbc,category));
+    qry->exec();
+    dbc.closeConnection();
+
+}
+
+Databasequery::~Databasequery()
+{
+
+}
+
+
